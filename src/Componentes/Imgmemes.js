2bbccf97b20b395ee86a8949a043c53c7e2fadb3
Vicente Daniel Caldarone <vicente.caldarone@gmail.com>
2023-07-09 21:24:13 -0300

    modificacion de titulos

--- src/Componentes/Imgmemes.js
+++ src/Componentes/Imgmemes.js
@@ -51,19 +51,19 @@ const Imgmemes = () => {
 
     return (
     <div className='text-center'>
-        <h1 className='mt-5 mb-3 text-light'>Editá tu propio meme</h1>
+        <h1 className='mt-3 mb-3 text-light'>Creá tu propio meme</h1>
 
         <div className='row'>
             <div className='col-12 col-sm-6'>
-                <h2 className='mt-2 mb-3'>Escribí tu frase</h2>
-                <input onChange={textSupMeme} className="form-control w-75 m-auto d-block mb-2" type="text" placeholder="Texto superior"/>
-                <input onChange={textMedMeme} className="form-control w-75 m-auto d-block mb-2" type="text" placeholder="Texto medio"/>
-                <input onChange={textInfMeme} className="form-control w-75 m-auto d-block mb-2" type="text" placeholder="Texto inferior"/>
+                <h3 className='mt-2 mb-3'>Escribí tu ingeniosa frase</h3>
+                <textarea onChange={textSupMeme} className="form-control w-75 m-auto d-block mb-2" placeholder="Texto superior"></textarea>
+                <textarea onChange={textMedMeme} className="form-control w-75 m-auto d-block mb-2" placeholder="Texto medio"></textarea>
+                <textarea onChange={textInfMeme} className="form-control w-75 m-auto d-block mb-2" placeholder="Texto inferior"></textarea>
 
-                <h2 className='mt-2 mb-3'>Elegí un color para el texto</h2>
+                <h3 className='mt-2 mb-3'>Elegí un color para el texto</h3>
                 <input onChange={seleccionarColor} type="color" className="m-auto form-control form-control-color" id="color"></input>
 
-                <h2 className='mt-2 mb-3'>Elegí la imagen para tu meme</h2>
+                <h3 className='mt-2 mb-3'>Elegí la imagen para tu meme</h3>
                 <select onChange={seleccionarImg} className="form-select form-select-lg mb-3 w-75 m-auto" aria-label="Default select example">
                     <option>Selecciona una imagen</option>
                     {imagenes.map((img) => (
@@ -71,17 +71,17 @@ const Imgmemes = () => {
                     ))}                    
                 </select> 
 
-                <button type='button' onClick={descarga} className='btn btn-primary mt-4 mb-4'>Descargar meme</button>
             </div>
             <div className='col-12 col-sm-6'>
                 <figure id="exportar" className="position-relative">
                     <div>
-                        <p className='w-100 position-absolute top-0 h1 text-center' style={{ "color": `${textoColor}`}}>{textoSuperior}</p>
-                        <p className='w-100 position-absolute top-50 h1 translate-middletext-center' style={{ "color": `${textoColor}`}}>{textoMedio}</p>
-                        <p className='w-100 position-absolute bottom-0 h1 text-center' style={{ "color": `${textoColor}`}}>{textoInferior}</p>
+                        <pre className='w-100 position-absolute top-0 h1 text-center' style={{ "color": `${textoColor}`}}>{textoSuperior}</pre>
+                        <pre className='w-100 position-absolute top-50 h1 translate-middletext-center' style={{ "color": `${textoColor}`}}>{textoMedio}</pre>
+                        <pre className='w-100 position-absolute bottom-0 h1 text-center' style={{ "color": `${textoColor}`}}>{textoInferior}</pre>
                         <img src={imgMeme} className='figure-img img-fluid' alt='meme'/>
                     </div>
                 </figure>
+                <button type='button' onClick={descarga} className='btn btn-primary mt-4 mb-4'>Descargá tu meme</button>
             </div>
         </div>
 
